f4b04d752b89f2518147bda7c762f8f341575cee
Felipe Pereira <felipejiti@hotmail.com>
2023-10-05 21:58:23 -0300

    "Atividades"

---
+++ exercicio03/exe03.js
@@ -0,0 +1,16 @@
+var notas = [7.0,5.0,4.0];
+var media = (notas[0] + notas[1] + notas[2]) / 3;
+
+if (media > 4 && media < 7){
+    console.log("Você está na recuperação.");
+    let notaRec = 9;
+    let mediaFinal = (notaRec + media) / 2;
+    if (mediaFinal >= 5){
+        console.log("Você está aprovado.")
+    } else{
+        console.log("Você foi reprovado.")
+    }
+} else if(media >= 7){
+    console.log("Você foi aprovado por média.")
+    typeof media
+}
